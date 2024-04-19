8c258434ecf870824742b96a734736b73f4a1384
antonijevicevadragana <antonijevicd.1991@gmail.com>
2024-04-19 11:27:19 +0200

    Router class

---
+++ Router.php
@@ -0,0 +1,105 @@
+<?php
+
+
+class Router
+{
+    protected $routes = [];
+
+    /**
+     * REGISTER ROUTE
+     * @param string $method
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+
+    public function registerRoute($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller
+        ];
+    }
+
+    /**
+     * Add a GET route
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+
+    public function Get($uri, $controller)
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    /**
+     * Add a POST route
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+
+    public function Post($uri, $controller)
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+
+    /**
+     * Add a PUT route
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+
+    public function Put($uri, $controller)
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+
+    /**
+     * Add a DELETE route
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+
+    public function Delete($uri, $controller)
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    /**
+     * Load error page
+     * @param int $code
+     * @return void
+     */
+
+     public function error($code = 404) {
+        http_response_code($code);
+        loadView('errors/' . $code);
+        exit;
+     }
+
+    /**
+     * Route the request
+     * @param $uri
+     * @param $method
+     * @return void
+     */
+
+    public function route($uri, $method) {
+
+        foreach($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] && $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+
+        $this->error();
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,16 +1,13 @@
 <?php require '../helpers.php';
 
-$routes = require '../routes.php';
+//$routes = require '../routes.php';
+
+require_once basePath('Router.php');
+$router= new Router();
+$routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-//inspectAndDie($uri);
-
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-}
-else {
-    require basePath($routes['404']);
-}
-
+$router->route($uri, $method);
 ?>
