b2e7f80832ae4357f67729d1034b001e2e23f2a1
antonijevicevadragana <antonijevicd.1991@gmail.com>
2024-05-22 09:33:34 +0200

    register user

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -18,7 +18,8 @@ class UserController
      * Show log in page
      * @return vodi
      */
-    public function login() {
+    public function login()
+    {
         loadView('users/login');
     }
 
@@ -26,7 +27,82 @@ class UserController
      * Shoe register page
      */
 
-    public function create() {
+    public function create()
+    {
         loadView('users/create');
+    }
+
+    /**
+     * Store user in database
+     * 
+     */
+    public function store()
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $password = $_POST['password'];
+        $confirmationPassword = $_POST['confirmation_password'];
+
+        $errors = [];
+        //Validation
+        if (!Validation::email($email)) {
+            $errors['email'] = 'Please enter a valid email address';
+        }
+
+        if (!Validation::string($name, 2, 50)) {
+            $errors['name'] = 'Name must be between 2 and 50 characters';
+        }
+
+        if (!Validation::string($password, 6, 50)) {
+            $errors['password'] = 'Password must be at least 6 characters';
+        }
+
+        if (!Validation::match($password, $confirmationPassword)) {
+            $errors['confirmation_password'] = 'Passwords do not match';
+        }
+
+        if (!empty($errors)) {
+            loadView('users/create', [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state
+                ]
+            ]);
+            exit;
+        } 
+            // Check if email exists
+    $params = [
+        'email' => $email
+      ];
+  
+      $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params)->fetch();
+  
+      if ($user) {
+        $errors['email'] = 'That email already exists';
+        loadView('users/create', [
+          'errors' => $errors
+        ]);
+        exit;
+      }
+  
+      // Create user account
+      $params = [
+        'name' => $name,
+        'email' => $email,
+        'city' => $city,
+        'state' => $state,
+        'password' => password_hash($password, PASSWORD_DEFAULT)
+      ];
+  
+      $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+  
+        redirect('/');
+
+
     }
 }
--- App/views/users/create.view.php
+++ App/views/users/create.view.php
@@ -7,12 +7,13 @@ loadPartials('nav');
         <div class="row justify-content-center frame">
             <h1 class="text-center">Register</h1>
             <hr>
-            <form method="POST" action="">
+            <?= loadPartials('errors', ['errors' => $errors ?? []])?>
+            <form method="POST" action="/auth/register">
             <div class="input-box">
-                    <input type="text" class="form-control" name="name" value="" placeholder="&#xf007; Name" style="font-family: Arial, FontAwesome">
+                    <input type="text" class="form-control" name="name" value="<?= $user['name'] ?? '' ?>" placeholder="&#xf007; Name" style="font-family: Arial, FontAwesome">
                 </div>
             <div class="input-box">
-                    <input type="email" class="form-control" name="email" value="" placeholder="&#xf0e0; Email" style="font-family: Arial, FontAwesome">
+                    <input type="email" class="form-control" name="email" value="<?= $user['email'] ?? '' ?>" placeholder="&#xf0e0; Email" style="font-family: Arial, FontAwesome">
                 </div>
              
                 <div class="input-box">
@@ -20,14 +21,14 @@ loadPartials('nav');
                 </div>
 
                 <div class="input-box">
-                    <input type="password" class="form-control" name="con_password" value="" placeholder="&#xf023; Confirm Password" style="font-family: Arial, FontAwesome">
+                    <input type="password" class="form-control" name="confirmation_password" value="" placeholder="&#xf023; Confirm Password" style="font-family: Arial, FontAwesome">
                 </div>
                 <div class="input-box">
-                    <input type="text" class="form-control" name="state" value="" placeholder="&#xf3c5; State" style="font-family: Arial, FontAwesome">
+                    <input type="text" class="form-control" name="state" value="<?= $user['state'] ?? '' ?>" placeholder="&#xf3c5; State" style="font-family: Arial, FontAwesome">
                 </div>
 
                 <div class="input-box">
-                    <input type="text" class="form-control" name="city" value="" placeholder="&#xf3c5; City" style="font-family: Arial, FontAwesome">
+                    <input type="text" class="form-control" name="city" value="<?= $user['city'] ?? '' ?>" placeholder="&#xf3c5; City" style="font-family: Arial, FontAwesome">
                 </div>
 
                 <div class="d-grid">
