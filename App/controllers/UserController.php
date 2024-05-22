03719d8365248a61442e0172c24b9ce0e2f52806
antonijevicevadragana <antonijevicd.1991@gmail.com>
2024-05-22 12:38:50 +0200

    logout and dynamic navbar

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -118,4 +118,19 @@ class UserController
     //inspectAndDie(Session::get('user'));
         redirect('/');
     }
+
+    /**
+     * Logout and kill session
+     * 
+     * @return void
+     */
+
+    public function logout() {
+       Session::clearAll();
+
+       $params = session_get_cookie_params();
+       setcookie('PHPSESSID', '', time() -86400, $params['path'], $params['domain']);
+
+       redirect('/');
+    }
 }
--- App/views/partials/nav.php
+++ App/views/partials/nav.php
@@ -1,27 +1,44 @@
-<nav class="navbar nav navbar-expand-lg p-3" id="navbar">
-    <a class="navbar-brand" href="/"> <span style="color:rgb(0, 149, 255)">&#60;</span> JobAds <span style="color:rgb(0, 149, 255)">/&#62;</span></a>
+<?php
+
+use Framework\Session;
+?>
+<nav class="navbar navbar-expand-lg p-3" id="navbar">
+    <a class="navbar-brand" href="/">
+        <span style="color:rgb(0, 149, 255)">&#60;</span> JobAds <span style="color:rgb(0, 149, 255)">/&#62;</span>
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-            
-
+        <ul class="navbar-nav me-auto"> 
             <li class="nav-item">
                 <a href="/listings" class="nav-link fs-5">All Job</a>
             </li>
         </ul>
-        <ul class="navbar-nav ms-auto"> <!-- ms auto for right align -->
-        <li class="nav-item">
-                <a href="/listings/create" class="nav-link fs-5 btnPostJob">Post Job</a>
-            </li>
-            <li class="nav-item">
-                <a href="/auth/login" class="nav-link fs-5">Login</a>
-            </li>
-            <li class="nav-item">
-                <a href="/auth/register" class="nav-link fs-5">Register</a>
-            </li>
+
+        <ul class="navbar-nav ms-auto d-flex align-items-center"> 
+            <?php if (Session::has('user')) : ?>
+                <li class="nav-item">
+                    <p class="p-2 fs-6 text-light mb-0">Welcome <?= Session::get('user')['name'] ?></p>
+                </li>
+
+                <li class="nav-item">
+                    <a href="/listings/create" class="nav-link fs-5 btnPostJob">Post Job</a>
+                </li>
+                <li class="nav-item">
+                    <form method="POST" action="/auth/logout">
+                        <button type="submit" class="nav-link fs-5">Logout</button>
+                    </form>
+                </li>
+            <?php else : ?>
+                <li class="nav-item">
+                    <a href="/auth/login" class="nav-link fs-5">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/auth/register" class="nav-link fs-5">Register</a>
+                </li>
+            <?php endif; ?>
         </ul>
     </div>
 </nav>
