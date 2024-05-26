ad74030719333726424c546064fb1a6edfac1922
antonijevicevadragana <antonijevicd.1991@gmail.com>
2024-05-26 12:57:12 +0200

    joblistings DataBase

--- joblistings.sql
+++ joblistings.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2024 at 11:42 AM
+-- Generation Time: May 26, 2024 at 12:29 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -58,6 +58,31 @@ INSERT INTO `listings` (`id`, `user_id`, `title`, `role_summary`, `description`,
 (4, 1, 'Data Analyst', 'We are hiring a Data Analyst to analyze and interpret data for insights', 'We are hiring a Data Analyst to analyze and interpret data for insights', '75000', 'data analysis, statistics', 'Data Insights LLC', '101 Data St', 'Chicago', 'IL', '4445555555', 'info@datainsights.com', 'Bachelors degree in Data Science or related field, strong analytical skills', 'Health benefits, remote work options, casual dress code', '2024-05-23', 'on-site', '2023-11-18 13:11:55'),
 (5, 2, 'Graphic Designer', 'We are hiring a Data Analyst to analyze and interpret data for insights', 'Join our creative team as a Graphic Designer and bring ideas to life', '70000', 'graphic design, creative', 'CreativeWorks Inc', '234 Design Blvd', 'Albany', 'NY', '4993219876', 'info@creativeworks.com', 'Bachelors degree in Graphic Design or related field, proficiency in Adobe Creative Suite', 'Flexible work hours, creative work environment, opportunities for growth', '2024-05-20', 'remote', '2023-11-18 13:13:35');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `city`, `state`, `created_at`) VALUES
+(1, 'John Doe', 'user1@gmail.com', '$2y$10$UkdJDaWLRHPVwOu3lb9XW.FZWZmFaLM0BJbaj0/7dvPIqs7sdDlvK', 'Boston', 'MA', '2023-11-18 12:55:59'),
+(2, 'Jane Doe', 'user2@gmail.com', '$2y$10$UkdJDaWLRHPVwOu3lb9XW.FZWZmFaLM0BJbaj0/7dvPIqs7sdDlvK', 'San Francisco', 'CA', '2023-11-18 12:58:26'),
+(3, 'Steve Smith', 'user3@gmail.com', '$2y$10$UkdJDaWLRHPVwOu3lb9XW.FZWZmFaLM0BJbaj0/7dvPIqs7sdDlvK', 'Chicago', 'IL', '2023-11-18 12:59:13');
+
 --
 -- Indexes for dumped tables
 --
@@ -70,6 +95,12 @@ ALTER TABLE `listings`
   ADD KEY `fk_listings_users_idx` (`user_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -78,6 +109,12 @@ ALTER TABLE `listings`
 --
 ALTER TABLE `listings`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
